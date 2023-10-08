ad7747241c523eb66a505ff1eaaa70e7108f7d57
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-08 17:32:57 +0600

    Added default colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        theme: '#ce1446',
+        body: '#515154',
+        heading: '#2d373c',
+        'secondary-bg': '#fafafa',
+      },
+    },
   },
   plugins: [],
 };
