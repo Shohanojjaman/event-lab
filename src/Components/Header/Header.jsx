661d8ee95de7f4111c4867f83edf2c89d3fa458c
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-08 18:40:10 +0600

    Created Header component and added to main layout.

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,76 @@
+import { Link, NavLink } from 'react-router-dom';
+
+const Header = () => {
+  const logo = 'https://i.ibb.co/TmYqw6T/logo.png';
+  const links = (
+    <>
+      <li>
+        <NavLink
+          to={'/'}
+          className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active-nav' : 'nav-link')}>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={'/about'}
+          className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active-nav' : 'nav-link')}>
+          About
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={'/events'}
+          className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active-nav' : 'nav-link')}>
+          Events
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to={'/Blog'}
+          className={({ isActive, isPending }) => (isPending ? 'pending' : isActive ? 'active-nav' : 'nav-link')}>
+          Blog
+        </NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="container mx-auto max-sm:px-5">
+      <div className="navbar items-center py-4">
+        <div className="navbar-start">
+          <Link to={'/'}>
+            <img src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="navbar-end">
+          <ul className="menu menu-horizontal px-1 gap-5 mr-6 hidden lg:flex">{links}</ul>
+          <div className="dropdown dropdown-left">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              {links}
+              <Link to={'/login'} className="primary-btn">
+                Log in
+              </Link>
+            </ul>
+          </div>
+          <Link to={'/login'} className="primary-btn hidden lg:inline">
+            Log in
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/Layout/Mainlayout.jsx
+++ src/Layout/Mainlayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../Components/Header/Header';
 
 const Mainlayout = () => {
   return (
     <main>
+        <Header></Header>
       <Outlet></Outlet>
     </main>
   );
