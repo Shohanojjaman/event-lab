6a083d12c14aba9c4058ddc312c36e8cac9975cd
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-09 11:15:14 +0600

    Customized container padding.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    container: {
+      padding: {
+        sm: '0, 1.25rem',
+      },
+    },
     extend: {
       colors: {
         theme: '#ce1446',
