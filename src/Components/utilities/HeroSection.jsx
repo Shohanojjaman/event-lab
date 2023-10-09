ca2edab0cf804b0d79273c8f2fee4a247cbd7110
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-09 13:19:00 +0600

    Reponsive hero section.

--- src/Components/utilities/HeroSection.jsx
+++ src/Components/utilities/HeroSection.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 const HeroSection = () => {
   return (
     <div className="hero-area py-28">
-      <div className="container mx-auto flex items-center h-full">
-        <div className="w-1/2 flex flex-col items-center h-full gap-6" data-aos="fade-right">
-          <h1 className="text-[65px] font-extrabold">
+      <div className="container mx-auto flex max-lg:flex-col space-y-12 items-center h-full">
+        <div className="lg:w-1/2 flex flex-col items-center h-full gap-6" data-aos="fade-right">
+          <h1 className="text-5xl md:text-[65px] leading-tight max-lg:text-center font-extrabold">
             Seamless Corporate Event Solutions with <span className="text-theme">E</span>vent{' '}
             <span className="text-theme">L</span>ab
           </h1>
-          <div className="flex self-start gap-4" data-aos="fade-up">
+          <div className="flex lg:self-start gap-4 flex-wrap justify-center" data-aos="fade-up">
             <Link to={'/about'} className="primary-btn">
               About Us
             </Link>
@@ -18,7 +18,7 @@ const HeroSection = () => {
             </Link>
           </div>
         </div>
-        <div className="w-1/2 flex justify-end" data-aos="fade-left">
+        <div className="lg:w-1/2 flex justify-center lg:justify-end" data-aos="fade-left">
           <img src={'https://i.ibb.co/GPVpKhz/hero-figure.png'} alt="" className="" />
         </div>
       </div>
