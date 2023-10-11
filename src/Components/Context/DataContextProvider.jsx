6a7c095c8e6e28641a379512ef9680a0e6e31dce
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-11 17:58:26 +0600

    Added Blogs loader on data context.

--- src/Components/Context/DataContextProvider.jsx
+++ src/Components/Context/DataContextProvider.jsx
@@ -4,14 +4,25 @@ import { createContext, useEffect, useState } from 'react';
 export const DataContext = createContext(null);
 const DataContextProvider = ({ children }) => {
   const [events, setEvents] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+
+  // Event data loader
   useEffect(() => {
     fetch('/data/events.json')
       .then((res) => res.json())
       .then((data) => setEvents(data))
       .catch((err) => console.error(err));
   }, []);
-  console.log(events);
-  const dataInfo = { events };
+
+  // Blog data loader
+  useEffect(() => {
+    fetch('/data/blogs.json')
+      .then((res) => res.json())
+      .then((data) => setBlogs(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const dataInfo = { events, blogs };
   return <DataContext.Provider value={dataInfo}>{children}</DataContext.Provider>;
 };
 
