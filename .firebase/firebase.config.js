fcf5bb3f57ca6a424cfe2885b2bccc87bf6520a4
Shohanojjaman <shohanojjamanemon@gmail.com>
2023-10-11 23:48:22 +0600

    Modified Firebase config file.

--- .firebase/firebase.config.js
+++ .firebase/firebase.config.js
@@ -1,16 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.local.FIREBASE_APIKEY,
-  authDomain: import.meta.env.local.FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.local.FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.local.FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.local.FIREBASE_MESSAGINGSENDERID,
-  appId: import.meta.env.local.FIREBASE_APPID,
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
